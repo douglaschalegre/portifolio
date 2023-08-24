b89626ea4af25941ad80dc36e55a73e765b6a819
Douglas Chalegre <douglas.torres@lccv.ufal.br>
2023-08-24 00:51:04 -0300

    chore: update next version

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
+  // experimental: {
+  //   appDir: true,
+  // },
   images: {
     domains: ['images.unsplash.com', 'tailwindui.com'],
     dangerouslyAllowSVG: true,
