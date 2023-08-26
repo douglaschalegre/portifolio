f2368fb737376cf4db6b5388587cc4e056019795
douglaschalegre <douglas.chalegre@gmail.com>
2023-08-26 01:53:30 -0300

    feature: Adding hovers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,10 @@ export default function Home() {
           <div className="hidden sm:mb-8 sm:flex sm:justify-center">
             <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-300 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
               Eu construo softwares daora utilizando tecnologias modernas.{' '}
-              <a href="#sobre" className="font-semibold text-orange-500">
+              <a
+                href="#sobre"
+                className="font-semibold text-orange-500 hover:text-orange-500/90"
+              >
                 <span className="absolute inset-0" aria-hidden="true" />
                 Saiba mais <span aria-hidden="true">&rarr;</span>
               </a>
@@ -40,13 +43,13 @@ export default function Home() {
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a
                 href="#redes"
-                className="rounded-md bg-orange-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-orange-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-500"
+                className="rounded-md bg-orange-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-orange-500/90 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-500"
               >
                 Fale comigo
               </a>
               <a
                 href="#sobre"
-                className="text-sm font-semibold leading-6 text-white"
+                className="text-sm font-semibold leading-6 text-white hover:text-white/90"
               >
                 Saiba mais <span aria-hidden="true">→</span>
               </a>
