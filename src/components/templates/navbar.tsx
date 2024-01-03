7b6498b3e38e770c7361c4deb241634cf39e4800
douglaschalegre <douglas.chalegre@gmail.com>
2024-01-03 03:05:06 -0300

    feature: adds new links to mobile hamburguer menu

--- src/components/templates/navbar.tsx
+++ src/components/templates/navbar.tsx
@@ -134,6 +134,18 @@ export default function Navbar() {
                     {item.name}
                   </a>
                 ))}
+                <div>
+                  <Separator className="bg-orange-500" />
+                </div>
+                {apps.map((item) => (
+                  <a
+                    key={item.name}
+                    href={item.href}
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-gray-50 hover:text-black"
+                  >
+                    {item.name}
+                  </a>
+                ))}
               </div>
               <div className="py-6">
                 <a
