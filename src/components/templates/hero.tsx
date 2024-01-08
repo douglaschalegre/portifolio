74a660565bdb1abb0f2aa047072d83caca8f6116
douglaschalegre <douglas.chalegre@gmail.com>
2024-01-07 22:37:38 -0300

    feature: Adding content about talks

--- src/components/templates/hero.tsx
+++ src/components/templates/hero.tsx
@@ -1,6 +1,6 @@
 export default function Hero() {
   return (
-    <div className="relative isolate px-6 pt-14 lg:px-8">
+    <div id="hero" className="px-6 pt-14 lg:px-8">
       <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
         <div className="hidden sm:mb-8 sm:flex sm:justify-center">
           <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-300 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
--- src/components/templates/talks.tsx
+++ src/components/templates/talks.tsx
@@ -1,9 +1,34 @@
 import { FaArrowCircleDown } from 'react-icons/fa';
 
 export default function Talks() {
+  const talks = [
+    {
+      name: 'Workshop Web',
+      description: 'Workshop de desenvolvimento web realizado na UNIT-AL.',
+      date: '15/08/2019',
+      url: 'https://github.com/douglaschalegre/Talks/tree/master/Workshop%20desenvolvimento%20web',
+      photo: '/talks/workshop-web.jpg',
+    },
+    {
+      name: 'Workshop Hands-on Twitter Clone',
+      description:
+        'Workshop de desenvolvimento fullstack de uma aplicação clone do Twitter realizado na FAN/FGV em Maceió.',
+      date: '30/03/2022',
+      url: 'https://github.com/douglaschalegre/Twitter-Clone',
+      photo: '/talks/workshop-twitter-clone.jpg',
+    },
+    {
+      name: 'Workshop Hands-on IA classificadora de cores',
+      description:
+        'Workshop de desenvolvimento de uma aplicação que classifica cores utilizando inteligência artificial e front-end para interação. Realizado na semana de extensão da UNIT-AL.',
+      date: '21/11/2022',
+      url: 'https://github.com/douglaschalegre/colors-collector',
+      photo: '/talks/workshop-ia-cores.jpg',
+    },
+  ];
   return (
-    <div id="talks" className="bg-black">
-      <div className="mx-auto max-w-2xl">
+    <div id="talks" className="px-6 pt-14 lg:px-8">
+      <div className="mx-auto max-w-2xl py-8 sm:py-48 lg:py-56">
         <div className="hidden sm:mb-8 sm:flex sm:justify-center"></div>
         <div className="text-center">
           <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
