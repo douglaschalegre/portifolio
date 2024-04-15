1e405725cc762339d94dfcb5f5b20a9ad7286114
douglaschalegre <douglas.chalegre@gmail.com>
2024-04-14 22:40:26 -0300

    feature: Add @vercel/analytics package

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,17 @@
 import './globals.css';
 import Navbar from '@/components/templates/navbar';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <html lang="en">
       <head />
       <body>
+        <Analytics />
         <Navbar />
         {children}
       </body>
